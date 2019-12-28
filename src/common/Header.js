e17569a0f2a767dd5b4740b0db1c3e371ee32614
zx88cvb <295389754@qq.com>
2019-12-28 16:48:08 +0800

    redux

---
+++ src/common/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Header'
+export default function Header(props) {
+  return (
+    <div></div>
+  );
+}
--- src/index/store.js
+++ src/index/store.js
@@ -10,7 +10,15 @@ import thunk from 'redux-thunk';
 export default createStore(
   combineReducers(reducers),
   {
-
+    from: '北京',
+    to: '上海',
+    isCitySelectorVisible: false,
+    currentSelectingLeftCity: false,
+    cityData: null,
+    // 当前是否加载城市数据
+    isLoadingCityData: false,
+    isDateSelectorVisible: false,
+    highSpeed: false
   },
   applyMiddleware(thunk)
 );
