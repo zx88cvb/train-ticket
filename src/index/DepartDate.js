1da8159e7b9fdfe49286ed616091a25e3b08800b
zx88cvb <295389754@qq.com>
2020-01-15 18:08:58 +0800

    8-9

--- src/index/DepartDate.js
+++ src/index/DepartDate.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {
+  useMemo
+} from 'react';
 import './DepartDate.css';
+import dayjs from 'dayjs';
+import { h0 } from '../common/fp';
 
 export default function DepartDate(props) {
+  const {
+    time,
+    onClick
+  } = props;
+
+  const h0OfDepart = h0(time);
+
+  const departDateString = useMemo(() => {
+    return dayjs(h0OfDepart).format('YYYY-MM-DD');
+  }, [h0OfDepart]);
   return (
-    <div></div>
+    <div>
+      { departDateString }
+    </div>
   );
 }
